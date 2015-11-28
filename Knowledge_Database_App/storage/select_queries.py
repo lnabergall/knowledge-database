5d610dd8d6ed914f2f2595bfaf04c24e2ac91fe4
Lukas <Lukas>
2015-11-27 19:37:54 -0500

    Finished remaining functions and classes of Storage Query API

--- Knowledge_Database_App/storage/select_queries.py
+++ Knowledge_Database_App/storage/select_queries.py
@@ -2,24 +2,55 @@
 
 """
 
-from sqlalchemy.orm import joinedload, aliased
+from sqlalchemy.orm import joinedload
 from sqlalchemy.orm.exc import MultipleResultsFound, NoResultFound
+from sqlalchemy.orm.query import Query as _Query
 
 from . import orm_core as orm
 
 
+class Query(_Query):
+	"""Custom SQLAlchemy query class."""
+
+	def values(self):
+		"""
+		Return an iterable of all scalar element values from rows 
+		matched by this query.
+
+		:raise MultipleValuesFound: If result rows have more than 
+									one element.
+		"""
+		try:
+			return [x for (x,) in self.all()]
+		except ValueError as e:
+			raise MultipleValuesFound(str(e))
+
+
+class MultipleValuesFound(ValueError, MultipleResultsFound):
+	"""
+	Exception raised by :meth:'Query.values' when multiple value were
+	found in a single result row.
+	"""
+
+
 class SelectError(Exception):
-	pass
+	"""
+	General exception raised when a database query returns an invalid 
+	result.
+	"""
 
 
 class InputError(Exception):
-	pass
+	"""
+	General exception raised when function is called with invalid argument 
+	values.
+	"""
 
 
 def get_content_piece(content_id):
 	"""
 	Input: content_id.
-	Returns: ContentPiece instance.
+	Returns: 'ContentPiece' instance.
 	"""
 	session = orm.start_session()
 	try:
@@ -34,7 +65,7 @@ def get_content_piece(content_id):
 def get_alternate_names(content_id):
 	"""
 	Input: content_id.
-	Returns: list of Name instances.
+	Returns: list of 'Name' instances.
 	"""
 	session = orm.start_session()
 	alternate_names = session.query(orm.ContentPiece.alternate_names).filter(
@@ -44,11 +75,11 @@ def get_alternate_names(content_id):
 
 def get_accepted_edits(content_id=None, edit_id=None, user_id=None, 
 					   text_id=None, name_id=None, citation_id=None, 
-					   keyword_id=None):
+					   keyword_id=None, ip_address=None):
 	"""
 	Input: content_id, edit_id, user_id, text_id, name_id, citation_id,
-		   or keyword_id.
-	Returns: AcceptedEdit instance or list of AcceptedEdit instances.
+		   keyword_id, or ip_address.
+	Returns: 'AcceptedEdit' instance or list of 'AcceptedEdit' instances.
 	"""
 	session = orm.start_session()
 	if content_id is not None:
@@ -59,6 +90,9 @@ def get_accepted_edits(content_id=None, edit_id=None, user_id=None,
 		accepted_edits = session.query(orm.AcceptedEdit).join(
 			orm.User).filter(orm.User.user_id == user_id).order_by(
 			orm.AcceptedEdit.acc_timestamp).all()
+	elif ip_address is not None:
+		accepted_edits = session.query(orm.AcceptedEdit).filter(
+			orm.AcceptedEdit.author_type == ip_address).all()
 	elif text_id is not None:
 		accepted_edits = session.query(orm.AcceptedEdit).join(
 			orm.Text).filter(orm.Text.text_id == text_id).order_by(
@@ -79,8 +113,8 @@ def get_accepted_edits(content_id=None, edit_id=None, user_id=None,
 		try:
 			accepted_edit = session.query(orm.AcceptedEdit).filter(
 				orm.AcceptedEdit.edit_id == edit_id).one()
-		except NoResultFound, MultipleResultsFound:
-			raise SelectError()
+		except (NoResultFound, MultipleResultsFound) as e:
+			raise SelectError(str(e))
 		else:
 			return accepted_edit
 
@@ -89,11 +123,11 @@ def get_accepted_edits(content_id=None, edit_id=None, user_id=None,
 
 def get_rejected_edits(content_id=None, edit_id=None, user_id=None, 
 					   text_id=None, name_id=None, citation_id=None, 
-					   keyword_id=None):
+					   keyword_id=None, ip_address=None):
 	"""
 	Input: content_id, edit_id, user_id, text_id, name_id, citation_id,
-		   or keyword_id.
-	Returns: RejectedEdit instance or list of RejectedEdit instances.
+		   keyword_id, or ip_address.
+	Returns: 'RejectedEdit' instance or list of 'RejectedEdit' instances.
 	"""
 	session = orm.start_session()
 	if content_id is not None:
@@ -104,6 +138,9 @@ def get_rejected_edits(content_id=None, edit_id=None, user_id=None,
 		rejected_edits = session.query(orm.RejectedEdit).join(
 			orm.User).filter(orm.User.user_id == user_id).order_by(
 			orm.RejectedEdit.rej_timestamp).all()
+	elif ip_address is not None:
+		rejected_edits = session.query(orm.RejectedEdit).filter(
+			orm.RejectedEdit.author_type == ip_address).all()
 	elif text_id is not None:
 		rejected_edits = session.query(orm.RejectedEdit).join(
 			orm.Text).filter(orm.Text.text_id == text_id).order_by(
@@ -124,8 +161,8 @@ def get_rejected_edits(content_id=None, edit_id=None, user_id=None,
 		try:
 			rejected_edit = session.query(orm.RejectedEdit).filter(
 				orm.RejectedEdit.edit_id == edit_id).one()
-		except NoResultFound, MultipleResultsFound:
-			raise SelectError()
+		except (NoResultFound, MultipleResultsFound) as e:
+			raise SelectError(str(e))
 		else:
 			return rejected_edit
 	else:
@@ -137,7 +174,7 @@ def get_rejected_edits(content_id=None, edit_id=None, user_id=None,
 def get_user_votes(user_id):
 	"""
 	Input: user_id.
-	Returns: list of Vote instances.
+	Returns: list of 'Vote' instances.
 	"""
 	session = orm.start_session()
 	votes = session.query(orm.Vote).join(orm.User).filter(
@@ -146,10 +183,10 @@ def get_user_votes(user_id):
 
 
 def get_accepted_votes(content_id=None, edit_id=None, 
-					   vote_id=None, user_id=None):
+					   vote_id=None, user_id=None, ip_address=None):
 	"""
-	Input: content_id, edit_id, vote_id, or user_id.
-	Returns: Vote instance or list of Vote instances.
+	Input: content_id, edit_id, vote_id, user_id, or ip_address.
+	Returns: 'Vote' instance or list of 'Vote' instances.
 	"""
 	session = orm.start_session()
 	if content_id is not None:
@@ -159,33 +196,36 @@ def get_accepted_votes(content_id=None, edit_id=None,
 	elif user_id is not None:
 		votes = session.query(orm.Vote).join(orm.AcceptedEdit).join(
 			orm.User).filter(orm.User.user_id == user_id).all()
+	elif ip_address is not None:
+		votes = session.query(orm.Vote).join(orm.AcceptedEdit).filter(
+			orm.AcceptedEdit.author_type == ip_address).all()
 	elif edit_id is not None:
 		try:
 			vote = session.query(orm.Vote).join(orm.AcceptedEdit).filter(
 				orm.AcceptedEdit.edit_id == edit_id).one()
-		except NoResultFound, MultipleResultsFound:
-			raise SelectError()
+		except (NoResultFound, MultipleResultsFound) as e:
+			raise SelectError(str(e))
 		else:
 			return vote
 	elif vote_id is not None:
 		try:
 			vote = session.query(orm.Vote).filter(
 				orm.Vote.vote_id == vote_id).one()
-		except NoResultFound, MultipleResultsFound:
-			raise SelectError()
+		except (NoResultFound, MultipleResultsFound) as e:
+			raise SelectError(str(e))
 		else:
 			return vote
 	else:
-		raise InputError("No arguments!")
+		raise InputError("Invalid arguments!")
 
 	return votes
 
 
 def get_rejected_votes(content_id=None, edit_id=None, 
-					   vote_id=None, user_id=None):
+					   vote_id=None, user_id=None, ip_address=None):
 	"""
-	Input: content_id, edit_id, vote_id, or user_id.
-	Returns: Vote instance or list of Vote instances.
+	Input: content_id, edit_id, vote_id, user_id, or ip_address.
+	Returns: 'Vote' instance or list of 'Vote' instances.
 	"""
 	session = orm.start_session()
 	if content_id is not None:
@@ -195,31 +235,134 @@ def get_rejected_votes(content_id=None, edit_id=None,
 	elif user_id is not None:
 		votes = session.query(orm.Vote).join(orm.RejectedEdit).join(
 			orm.User).filter(orm.User.user_id == user_id).all()
+	elif ip_address is not None:
+		votes = session.query(orm.Vote).join(orm.RejectedEdit).filter(
+			orm.RejectedEdit.author_type == ip_address).all()
 	elif edit_id is not None:
 		try:
 			vote = session.query(orm.Vote).join(orm.RejectedEdit).filter(
 				orm.RejectedEdit.edit_id == edit_id).one()
-		except NoResultFound, MultipleResultsFound:
-			raise SelectError()
+		except (NoResultFound, MultipleResultsFound) as e:
+			raise SelectError(str(e))
 		else:
 			return vote
 	elif vote_id is not None:
 		try:
 			vote = session.query(orm.Vote).filter(
 				orm.Vote.vote_id == vote_id).one()
-		except NoResultFound, MultipleResultsFound:
-			raise SelectError()
+		except (NoResultFound, MultipleResultsFound) as e:
+			raise SelectError(str(e))
 		else:
 			return vote
 	else:
-		raise InputError("No arguments!")
+		raise InputError("Invalid arguments!")
 
 	return votes
 
 
-def get_user_encrypt_info(email=None):
-	
+def get_user_encrypt_info(email):
+	"""
+	Input: email.
+	Returns: tuple (pass_hash_type, pass_salt, remember_token_hash).
+	"""
+	session = orm.start_session()
+	try:
+		encrypt_info = session.query(orm.User.pass_hash_type, 
+			orm.User.pass_salt, orm.User.remember_token_hash).filter(
+			orm.User.email == email).one()
+	except (NoResultFound, MultipleResultsFound) as e:
+		raise SelectError(str(e))
+	else:
+		return encrypt_info
 
-def get_user(user_id=None, email=None, pass_hash=None, remember_id=None, 
+
+def get_user(email=None, pass_hash=None, remember_id=None, 
 			 remember_token_hash=None):
-	
+	"""
+	Input: email and pass_hash or remember_id and remember_token_hash.
+	Returns: 'User' instance.
+	"""
+	session = orm.start_session()
+	if email is not None and pass_hash is not None:
+		try:
+			user = session.query(orm.User).filter(orm.User.email == email, 
+				orm.User.pass_hash == pass_hash).one()
+		except MultipleResultsFound as e:
+			raise SelectError(str(e))
+		except NoResultFound:
+			return None
+	elif remember_id is not None and remember_token_hash is not None:
+		try:
+			user = session.query(orm.User).filter(
+				orm.User.remember_id == remember_id, 
+				orm.User.remember_token_hash == remember_token_hash).one()
+		except MultipleResultsFound as e:
+			raise SelectError(str(e))
+		except NoResultFound:
+			return None
+	else:
+		raise InputError("Invalid arguments!")
+
+	return user
+
+
+def get_user_emails(content_id=None, accepted_edit_id=None, 
+					rejected_edit_id=None):
+	"""
+	Input: content_id or edit_id.
+	Returns: email or list of emails.
+	"""
+	session = orm.start_session()
+	if content_id is not None:
+		try:
+			emails = session.query(orm.User.email).join(
+				orm.ContentPiece, orm.User.pieces).filter(
+				orm.ContentPiece.content_id == content_id).values()
+		except MultipleValuesFound as e:
+			raise SelectError(str(e))
+		else:
+			return emails
+	elif accepted_edit_id is not None:
+		try:
+			email = session.query(orm.User.email).join(
+				orm.AcceptedEdit).filter(orm.AcceptedEdit.edit_id 
+				== accepted_edit_id).one()
+		except (NoResultFound, MultipleResultsFound) as e:
+			raise SelectError(str(e))
+		else:
+			return email
+	else:
+		raise InputError("Invalid arguments!")
+
+
+def get_user_reports(content_id=None, report_id=None, 
+					 user_id=None, admin_id=None, ip_address=None):
+	"""
+	Input: content_id, report_id, user_id, admin_id, or ip_address.
+	Returns: 'UserReport' instance or list of 'UserReport' instances.
+	"""
+	session = orm.start_session()
+	if content_id is not None:
+		reports = session.query(orm.UserReport).join(orm.ContentPiece).filter(
+			orm.ContentPiece.content_id == content_id).all()
+	elif user_id is not None:
+		reports = session.query(orm.UserReport).join(orm.User, 
+			orm.UserReport.author).filter(orm.User.user_id == user_id).all()
+	elif admin_id is not None:
+		reports = session.query(orm.UserReport).join(orm.User, 
+			orm.UserReport.admin).filter(orm.User.user_id == admin_id).all()
+	elif ip_address is not None:
+		reports = session.query(orm.UserReport).filter(
+			orm.UserReport.author_type == ip_address).all()
+	elif report_id is not None:
+		try:
+			report = session.query(orm.UserReport).filter(
+				orm.UserReport.report_id == report_id).one()
+		except (NoResultFound, MultipleResultsFound) as e:
+			raise SelectError(str(e))
+		else:
+			return report
+	else:
+		raise InputError("Invalid arguments!")
+
+	return reports
