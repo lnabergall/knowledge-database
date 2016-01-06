4967a836e4c321c7e63bf91c7df7fc92cb100c6c
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-01-06 02:05:51 -0500

    Started Content Edit API layout

--- Knowledge_Database_App/content/edit.py
+++ Knowledge_Database_App/content/edit.py
@@ -1,1 +1,45 @@
+"""
+Content Edit API
+"""
 
+from datetime import datetime
+
+from Knowledge_Database_App import email
+from Knowledge_Database_App.storage import (orm_core as orm,
+                                            select_queries as select,
+                                            action_queries as action)
+from .celery import celery_app
+from .content import Content, Name, Text, UserData
+
+
+class Edit:
+
+    def __init__(self):
+        pass
+
+    @classmethod
+    def bulk_retrieve(cls):
+        pass
+
+    def start_vote(self):
+        pass
+
+    def save(self):
+        pass
+
+    def validate(self):
+        pass
+
+    def accept(self):
+        pass
+
+    def reject(self):
+        pass
+
+    @celery_app.task(name="edit._notify")
+    def _notify(self):
+        pass
+
+    @property
+    def json_ready(self):
+        pass
