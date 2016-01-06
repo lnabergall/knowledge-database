386fd12b023374d856df37e0fd65b6e4de2f31a2
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-01-06 02:05:02 -0500

    Added edit notification task route setting

--- Knowledge_Database_App/content/celery_config.py
+++ Knowledge_Database_App/content/celery_config.py
@@ -12,6 +12,10 @@ BROKER_URL = "amqp://"
 CELERY_RESULT_BACKEND = "redis://"
 CELERY_TASK_RESULT_EXPIRES = 259200     # 3 days
 
+# Ignore results, but keep errors in Redis
+CELERY_IGNORE_RESULT = True
+CELERY_STORE_ERRORS_EVEN_IF_IGNORED = True
+
 # List of modules to import when celery starts.
 CELERY_IMPORTS = []
 
@@ -24,9 +28,13 @@ CELERY_QUEUES = (
 )
 CELERY_DEFAULT_QUEUE = "default"
 
-# Ignore results, but keep errors in Redis
-CELERY_IGNORE_RESULT = True
-CELERY_STORE_ERRORS_EVEN_IF_IGNORED = True
+# Task routes.
+CELERY_ROUTES = {
+    "edit._notify": {
+        "queue": "email_tasks",
+        "routing_key": "email.edit._notify"
+    }
+}
 
 # File to store worker state (e.g. revoked tasks).
 CELERYD_STATE_DB = "celery_worker_state.txt"
