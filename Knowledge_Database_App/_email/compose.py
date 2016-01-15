4289a9f2fc568e635439c38a3dde9a9a0c856a80
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-01-14 22:31:57 -0500

    Added email format for notifying authors of the acceptance or rejection of an edit

--- Knowledge_Database_App/_email/compose.py
+++ Knowledge_Database_App/_email/compose.py
@@ -32,11 +32,14 @@ class Email:
         self.recipient_address = user_address
         self.message = Message()
         self.message["To"] = self.recipient_address
-        if edit_text is not None and days_remaining is None:
+        if edit_text is not None and days_remaining is None and vote_result is None:
             self._compose_edit_alert(user_name, content_name, edit_text)
-        elif edit_text is not None:
+        elif edit_text is not None and vote_result is None:
             self._compose_vote_reminder(user_name, content_name,
                                        edit_text, days_remaining)
+        elif edit_text is not None:
+            self._compose_author_validated_alert(user_name, content_name,
+                                                 edit_text, vote_result)
         elif vote_result is not None:
             self._compose_edit_validated_alert(user_name,
                                               content_name, vote_result)
@@ -65,14 +68,14 @@ class Email:
     def _compose_vote_reminder(self, user_name, content_name,
                               edit_text, days_remaining):
         self.message["Subject"] = "Remember to vote!"   # TEMPORARY
-        with open("templates\vote_reminder.html", "r") as html_file:
+        with open(r"templates\vote_reminder.html", "r") as html_file:
             rich_text = html_file.read().format(
                 user_name=user_name,
                 content_name=content_name,
                 edit_text=edit_text,
                 days_remaining=days_remaining,
             )
-        with open("templates\vote_reminder_plain.txt", "r") as text_file:
+        with open(r"templates\vote_reminder_plain.txt", "r") as text_file:
             plain_text = text_file.read().format(
                 user_name=user_name,
                 content_name=content_name,
@@ -86,7 +89,6 @@ class Email:
 
     def _compose_edit_validated_alert(self, user_name,
                                      content_name, vote_result):
-
         if vote_result:
             self.message["Subject"] = "Your edit has been accepted!"  # TEMPORARY
             with open("templates\edit_accepted.html", "r") as html_file:
@@ -115,6 +117,46 @@ class Email:
                 plain_text = text_file.read().format(
                     user_name=user_name,
                     content_name=content_name,
+                )
+            texts = MIMEMultipart()
+            texts.attach(MIMEText(rich_text, "html"))
+            texts.attach(MIMEText(plain_text))
+            self.message.attach(texts)
+
+    def _compose_author_validated_alert(self, user_name, content_name,
+                                        edit_text, vote_result):
+        if vote_result:
+            self.message["Subject"] = "An edit has been accepted!"  # TEMPORARY
+            with open("templates\author_edit_accepted.html", "r") as html_file:
+                rich_text = html_file.read().format(
+                    user_name=user_name,
+                    content_name=content_name,
+                    edit_text=edit_text,
+                )
+            with open("templates\author_edit_accepted_plain.txt", "r") as text_file:
+                plain_text = text_file.read().format(
+                    user_name=user_name,
+                    content_name=content_name,
+                    edit_text=edit_text,
+                )
+            texts = MIMEMultipart()
+            texts.attach(MIMEText(rich_text, "html"))
+            texts.attach(MIMEText(plain_text))
+            self.message.attach(texts)
+        else:
+            # TEMPORARY
+            self.message["Subject"] = "There was a problem with your edit..."
+            with open("templates\author_edit_rejected.html", "r") as html_file:
+                rich_text = html_file.read().format(
+                    user_name=user_name,
+                    content_name=content_name,
+                    edit_text=edit_text,
+                )
+            with open("templates\author_edit_rejected_plain.txt", "r") as text_file:
+                plain_text = text_file.read().format(
+                    user_name=user_name,
+                    content_name=content_name,
+                    edit_text=edit_text,
                 )
             texts = MIMEMultipart()
             texts.attach(MIMEText(rich_text, "html"))
