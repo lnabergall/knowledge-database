a57bf1d8871ab7426cf1fce50036a6375740c142
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-01-16 02:21:57 -0500

    Started Content Vote API

--- Knowledge_Database_App/content/vote.py
+++ Knowledge_Database_App/content/vote.py
@@ -1,1 +1,22 @@
+"""
+Content Vote API
+"""
 
+from Knowledge_Database_App.storage import (orm_core as orm,
+                                            select_queries as select,
+                                            action_queries as action)
+from . import redis
+from .content import UserData
+
+
+class AuthorVote:
+
+    storage_handler = orm.StorageHandler()
+
+    content_id = None   # Integer.
+    edit_id = None      # Integer.
+    vote = None         # String, expects 'Y' or 'N'.
+    author = None       # UserData object.
+
+    def __init__(self, content_id, edit_id, vote, author_id):
+        pass
