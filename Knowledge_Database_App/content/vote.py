bdc49e670e05d7b24dabb7d50370154691f17b5e
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-01-17 03:05:50 -0500

    Completed Content Vote API

--- Knowledge_Database_App/content/vote.py
+++ Knowledge_Database_App/content/vote.py
@@ -9,7 +9,10 @@ Classes:
 
     AuthorVote
 """
+
 from datetime import datetime
+
+import dateutil.parser as dateparse
 
 from Knowledge_Database_App.storage import (orm_core as orm,
                                             select_queries as select,
@@ -30,27 +33,37 @@ class AuthorVote:
     storage_handler = orm.StorageHandler()
 
     edit_id = None      # Integer.
-    vote_status = None  # String, expects 'in-progress' or 'completed'.
+    vote_status = None  # String, expects 'in-progress' or 'ended'.
     vote = None         # String, expects 'Y' or 'N'.
     timestamp = None    # Datetime.
     author = None       # UserData object.
 
-    def __init__(self, vote_status, edit_id, vote, voter_id):
+    def __init__(self, vote_status, edit_id, vote, voter_id, timestamp=None):
         if (not content_id or not edit_id or
                 (vote != "Y" and vote != "N") or not voter_id or
                 (vote_status is not None and vote_status != "in-progress"
-                and vote_status != "completed")):
+                and vote_status != "ended")):
             raise select.InputError("Invalid arguments!")
         else:
             self.edit_id = edit_id
             self.vote_status = vote_status
             self.vote = vote
-            self.timestamp = datetime.utcnow()
+            self.timestamp = (datetime.utcnow() if timestamp is None
+                              else timestamp)
             self.author = UserData(user_id=voter_id)
 
     @classmethod
-    def unpack_vote_summary(cls):
-        pass
+    def unpack_vote_summary(cls, vote_summary):
+        vote_lists = [
+            vote_string[:-3].split(", ")
+            if vote_string.endswith(">, ") else vote_string[:-1].split(", ")
+            for vote_string in vote_summary.split("<")
+        ]
+        vote_dict = {
+            vote_list[0]: vote_list[1] + str("; ") + vote_list[2]
+            for vote_list in vote_lists[1:]
+        }
+        return vote_dict
 
     @classmethod
     def _retrieve_from_storage(cls, edit_id=None, vote_id=None,
@@ -92,19 +105,49 @@ class AuthorVote:
             return vote_dict
 
     @classmethod
-    def bulk_retrieve(cls, edit_id=None, vote_id=None, vote_status=None):
-        pass
+    def bulk_retrieve(cls, edit_id=None, vote_id=None,
+                      vote_status=None, validation_status=None):
+        if vote_status == "in-progress":
+            if edit_id is None:
+                raise select.InputError("Invalid argument!")
+            else:
+                vote_dict = cls._retrieve_from_redis(edit_id)
+                votes = [AuthorVote(vote_status, edit_id, value[0],
+                                    key, timestamp=dateparse.parse(value[3:]))
+                         for key, value in vote_dict.items()]
+        elif vote_status == "ended":
+            if edit_id is not None:
+                vote_object = cls._retrieve_from_storage(
+                    edit_id=edit_id, validation_status=validation_status)
+            elif vote_id is not None:
+                vote_object = cls._retrieve_from_storage(
+                    vote_id=vote_id, validation_status=validation_status)
+            else:
+                raise select.InputError("Invalid arguments!")
+            vote_dict = cls.unpack_vote_summary(vote_object.vote)
+            votes = [AuthorVote(vote_status, edit_id, value[0],
+                                    key, timestamp=dateparse.parse(value[3:]))
+                         for key, value in vote_dict.items()]
+        else:
+            raise select.InputError("Invalid argument!")
+
+        return votes
 
     @classmethod
-    def get_vote_summary(cls, edit_id=None, votes=None):
-        pass
+    def get_vote_summary(cls, votes):
+        vote_summary = ""
+        for vote in votes:
+            vote_summary += ("<" + vote.vote + ", " + str(vote.author.user_id) +
+                             ", " + str(vote.timestamp) + ">, ")
+        return vote_summary[:-2]
 
     def save(self):
         if self.vote_status != "in-progress":
             raise NotImplementedError
         else:
             try:
-                redis.store_vote(self.edit_id, self.author.user_id, self.vote)
+                redis.store_vote(self.edit_id, self.author.user_id,
+                                 self.vote + "; " + str(self.timestamp))
             except redis.DuplicateVoteError:
                 raise redis.DuplicateVoteError(
                     "Vote already submitted by user " +
