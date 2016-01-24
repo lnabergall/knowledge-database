05a054f3efa2cb99097687b23080c240fe5b80b4
Lukas <Lukas>
2016-01-24 03:26:42 -0500

    Implement recent_activity method

--- Knowledge_Database_App/content/content_view.py
+++ Knowledge_Database_App/content/content_view.py
@@ -2,6 +2,8 @@
 Content Piece View API
 """
 
+from datetime import datetime
+
 from .content import Content
 from .edit import Edit
 from .vote import AuthorVote
@@ -100,8 +102,88 @@ class ContentView:
             return completions
 
     @classmethod
-    def recent_activity(cls):
-        pass
+    def recent_activity(cls, user_id, page_num=1):
+        try:
+            content_ids = Content.bulk_retrieve(user_id=user_id, ids_only=True)
+            validating_edits = Edit.bulk_retrieve(
+                "validating", content_ids=content_ids)
+            accepted_edits = Edit.bulk_retrieve(
+                "accepted", content_ids=content_ids)
+            rejected_edits = Edit.bulk_retrieve(
+                "rejected", content_ids=content_ids)
+            user_rejected_edits = Edit.bulk_retrieve(
+                "rejected", user_id=user_id)
+        except:
+            raise
+        else:
+            user_rejected_edits = [edit for edit in user_rejected_edits
+                                   if edit.content_id not in content_ids]
+
+        def ordering_func(edit):
+            if edit is None:
+                return datetime.utcnow()
+            if edit.validation_status == "validating":
+                return edit.timestamp
+            else:
+                return edit.validated_timestamp
+
+        # Use ordering function to sort edits 
+        # in descending chronological order.
+        max_index = max(len(validating_edits), len(accepted_edits), 
+                        len(rejected_edits), len(user_rejected_edits))
+        validating_edits.reverse()
+        accepted_edits.reverse()
+        rejected_edits.reverse()
+        user_rejected_edits.reverse()
+        descending_edits = []
+        while True:
+            try:
+                validating_edit = validating_edits[-1]
+            except IndexError:
+                validating_edit = None
+            try:
+                accepted_edit = accepted_edits[-1]
+            except IndexError:
+                accepted_edit = None
+            try:
+                rejected_edit = rejected_edits[-1]
+            except IndexError:
+                rejected_edit = None
+            try:
+                user_rejected_edit = user_rejected_edits[-1]
+            except IndexError:
+                user_rejected_edit = None
+            if (validating_edit is None and accepted_edit is None
+                    and rejected_edit is None and user_rejected_edit is None):
+                break
+            next_oldest_edit = min(validating_edit, accepted_edit, 
+                                   rejected_edit, user_rejected_edit, 
+                                   key=ordering_func)
+            if next_oldest_edit.validation_status == "validating":
+                validating_edits.pop()
+            elif next_oldest_edit.validation_status == "accepted":
+                accepted_edits.pop()
+            else:
+                if next_oldest_edit.content_id in content_ids:
+                    rejected_edits.pop()
+                else:
+                    user_rejected_edits.pop()
+            descending_edits.append(next_oldest_edit)
+
+        # Now serialize and remove excess data.
+        for i in range(len(descending_edits)):
+            edit = descending_edits[i].json_ready
+            descending_edits[i] = {
+                "content_id": edit["content_id"],
+                "edit_id": edit["edit_id"],
+                "timestamp": edit["timestamp"],
+                "validated_timestamp": edit["validated_timestamp"],
+                "validation_status": edit["validation_status"],
+                "content_part": edit["content_part"],
+                "author_type": edit["author_type"],
+                "author": edit["author"],
+            }
+        return descending_edits
 
     @classmethod
     def validation_data(cls):
