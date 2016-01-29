ee84930ba5480ede6c1093b3876f2720f0c0015d
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-01-29 00:58:16 -0500

    Completed Content Vote View layer

--- Knowledge_Database_App/content/vote_view.py
+++ Knowledge_Database_App/content/vote_view.py
@@ -2,10 +2,27 @@
 Content Vote View API
 """
 
-from . import vote
+from .vote import AuthorVote
 
 
 class VoteView:
 
-    def __init__(self):
-        pass
+    def __init__(self, vote_status, edit_id, vote, voter_id,
+                 timestamp=None, close_timestamp=None):
+        """
+        Args:
+            vote_status: String, accepts 'in-progress' or 'ended'.
+            edit_id: Integer.
+            vote: String, expects 'Y' or 'N'.
+            voter_id: Integer.
+            timestamp: Datetime. Defaults to None.
+            close_timestamp: Datetime. Defaults to None.
+        """
+        try:
+            vote = AuthorVote(vote_status, edit_id, vote, voter_id,
+                              timestamp, close_timestamp)
+            vote.save()
+        except:
+            raise
+        else:
+            self.vote = vote.json_ready
