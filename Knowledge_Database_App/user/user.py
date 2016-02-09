170ad3aa21c2274e2c7002c84c5bdce113a2fb83
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-02-08 23:03:08 -0500

    Further layout

--- Knowledge_Database_App/user/user.py
+++ Knowledge_Database_App/user/user.py
@@ -4,6 +4,7 @@ User API
 
 from passlib.apps import custom_app_context
 
+from Knowledge_Database_App.content.celery import celery_app
 from Knowledge_Database_App.storage import (orm_core as orm,
                                             select_queries as select,
                                             action_queries as action)
@@ -23,4 +24,33 @@ class EmailAddressError(Exception):
 
 class RegisteredUser:
 
-    pass
+    def __init__(self):
+        pass
+
+    def _check_legal(self):
+        pass
+
+    def _transfer(self):
+        pass
+
+    def register(self):
+        pass
+
+    def store(self):
+        pass
+
+    @celery_app.task(name="user.send_welcome")
+    def send_welcome(self):
+        pass
+
+    @celery_app.task(name="user.request_confirm")
+    def request_confirm(self):
+        pass
+
+    @classmethod
+    def process_confirm(cls):
+        pass
+
+    @property
+    def json_ready(self):
+        return None
