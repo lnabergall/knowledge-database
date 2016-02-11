811ec26e70e8aec65c018cb795b8f908a1c77cc8
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-02-10 22:16:04 -0500

    Added pre-compilation of regexes

--- Knowledge_Database_App/user/user_config.py
+++ Knowledge_Database_App/user/user_config.py
@@ -2,5 +2,9 @@
 User authentication information requirement settings.
 """
 
-PASS_REGEX = r"(?!.*[\\\"])(?=.{8,160})(?=.*[a-zA-Z])(?:(?=.*[0-9])|(?=.*[!#$%&?])+)"
-USER_NAME = r"(?!.*[\\\"])(?:\S{2,}\s{1}\S{2,})+"
+import re
+
+
+PASS_REGEX = re.compile(
+    r"(?!.*[\\\"])(?=.{8,160})(?=.*[a-zA-Z])(?:(?=.*[0-9])|(?=.*[!#$%&?])+)")
+USER_NAME_REGEX = re.compile(r"(?!.*[\\\"])(?:\S{2,}\s{1}\S{2,})+")
