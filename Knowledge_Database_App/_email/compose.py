bc31878ac59a5536dfa77b413bc25f7925876e79
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-02-17 00:19:50 -0500

    Added confirmation emails

--- Knowledge_Database_App/_email/compose.py
+++ Knowledge_Database_App/_email/compose.py
@@ -27,36 +27,39 @@ class Email:
         message: Message object. This is the email intended for sending.
     """
 
-    def __init__(self, user_address, user_name, content_name,
-                 days_remaining=None, edit_text=None, vote_result=None):
+    def __init__(self, user_address, user_name, content_name=None,
+                 days_remaining=None, edit_text=None, vote_result=None,
+                 confirmation_id=None):
         self.recipient_address = user_address
+        self.user_name = user_name
         self.message = Message()
         self.message["To"] = self.recipient_address
-        if edit_text is not None and days_remaining is None and vote_result is None:
-            self._compose_edit_alert(user_name, content_name, edit_text)
+        if (edit_text is not None and
+                days_remaining is None and vote_result is None):
+            self._compose_edit_alert(content_name, edit_text)
         elif edit_text is not None and vote_result is None:
-            self._compose_vote_reminder(user_name, content_name,
-                                       edit_text, days_remaining)
+            self._compose_vote_reminder(content_name, edit_text, days_remaining)
         elif edit_text is not None:
-            self._compose_author_validated_alert(user_name, content_name,
-                                                 edit_text, vote_result)
+            self._compose_author_validated_alert(
+                content_name, edit_text, vote_result)
         elif vote_result is not None:
-            self._compose_edit_validated_alert(user_name,
-                                              content_name, vote_result)
+            self._compose_edit_validated_alert(content_name, vote_result)
+        elif confirmation_id is not None:
+            self._compose_confirmation_request(confirmation_id, days_remaining)
         else:
             raise InputError("Invalid arguments!")
 
-    def _compose_edit_alert(self, user_name, content_name, edit_text):
+    def _compose_edit_alert(self, content_name, edit_text):
         self.message["Subject"] = "New Edit!"   # TEMPORARY
         with open("templates\edit_alert.html", "r") as html_file:
             rich_text = html_file.read().format(
-                user_name=user_name,
+                user_name=self.user_name,
                 content_name=content_name,
                 edit_text=edit_text,
             )
         with open("templates\edit_alert_plain.txt", "r") as text_file:
             plain_text = text_file.read().format(
-                user_name=user_name,
+                user_name=self.user_name,
                 content_name=content_name,
                 edit_text=edit_text,
             )
@@ -65,19 +68,18 @@ class Email:
         texts.attach(MIMEText(plain_text))
         self.message.attach(texts)
 
-    def _compose_vote_reminder(self, user_name, content_name,
-                              edit_text, days_remaining):
+    def _compose_vote_reminder(self, content_name, edit_text, days_remaining):
         self.message["Subject"] = "Remember to vote!"   # TEMPORARY
         with open(r"templates\vote_reminder.html", "r") as html_file:
             rich_text = html_file.read().format(
-                user_name=user_name,
+                user_name=self.user_name,
                 content_name=content_name,
                 edit_text=edit_text,
                 days_remaining=days_remaining,
             )
         with open(r"templates\vote_reminder_plain.txt", "r") as text_file:
             plain_text = text_file.read().format(
-                user_name=user_name,
+                user_name=self.user_name,
                 content_name=content_name,
                 edit_text=edit_text,
                 days_remaining=days_remaining,
@@ -87,18 +89,17 @@ class Email:
         texts.attach(MIMEText(plain_text))
         self.message.attach(texts)
 
-    def _compose_edit_validated_alert(self, user_name,
-                                     content_name, vote_result):
+    def _compose_edit_validated_alert(self, content_name, vote_result):
         if vote_result:
             self.message["Subject"] = "Your edit has been accepted!"  # TEMPORARY
             with open("templates\edit_accepted.html", "r") as html_file:
                 rich_text = html_file.read().format(
-                    user_name=user_name,
+                    user_name=self.user_name,
                     content_name=content_name,
                 )
             with open("templates\edit_accepted_plain.txt", "r") as text_file:
                 plain_text = text_file.read().format(
-                    user_name=user_name,
+                    user_name=self.user_name,
                     content_name=content_name,
                 )
             texts = MIMEMultipart()
@@ -110,12 +111,12 @@ class Email:
             self.message["Subject"] = "There was a problem with your edit..."
             with open("templates\edit_rejected.html", "r") as html_file:
                 rich_text = html_file.read().format(
-                    user_name=user_name,
+                    user_name=self.user_name,
                     content_name=content_name,
                 )
             with open("templates\edit_rejected_plain.txt", "r") as text_file:
                 plain_text = text_file.read().format(
-                    user_name=user_name,
+                    user_name=self.user_name,
                     content_name=content_name,
                 )
             texts = MIMEMultipart()
@@ -123,19 +124,19 @@ class Email:
             texts.attach(MIMEText(plain_text))
             self.message.attach(texts)
 
-    def _compose_author_validated_alert(self, user_name, content_name,
+    def _compose_author_validated_alert(self, content_name,
                                         edit_text, vote_result):
         if vote_result:
             self.message["Subject"] = "An edit has been accepted!"  # TEMPORARY
             with open("templates\author_edit_accepted.html", "r") as html_file:
                 rich_text = html_file.read().format(
-                    user_name=user_name,
+                    user_name=self.user_name,
                     content_name=content_name,
                     edit_text=edit_text,
                 )
             with open("templates\author_edit_accepted_plain.txt", "r") as text_file:
                 plain_text = text_file.read().format(
-                    user_name=user_name,
+                    user_name=self.user_name,
                     content_name=content_name,
                     edit_text=edit_text,
                 )
@@ -148,13 +149,13 @@ class Email:
             self.message["Subject"] = "There was a problem with your edit..."
             with open("templates\author_edit_rejected.html", "r") as html_file:
                 rich_text = html_file.read().format(
-                    user_name=user_name,
+                    user_name=self.user_name,
                     content_name=content_name,
                     edit_text=edit_text,
                 )
             with open("templates\author_edit_rejected_plain.txt", "r") as text_file:
                 plain_text = text_file.read().format(
-                    user_name=user_name,
+                    user_name=self.user_name,
                     content_name=content_name,
                     edit_text=edit_text,
                 )
@@ -162,3 +163,22 @@ class Email:
             texts.attach(MIMEText(rich_text, "html"))
             texts.attach(MIMEText(plain_text))
             self.message.attach(texts)
+
+    def _compose_confirmation_request(self, confirmation_id, days_remaining):
+        self.message["Subject"] = "Please confirm your email!"   # TEMPORARY
+        with open(r"templates\email_confirmation.html", "r") as html_file:
+            rich_text = html_file.read().format(
+                user_name=self.user_name,
+                confirmation_id=confirmation_id,
+                days_remaining=days_remaining,
+            )
+        with open(r"templates\email_confirmation_plain.txt", "r") as text_file:
+            plain_text = text_file.read().format(
+                user_name=self.user_name,
+                confirmation_id=confirmation_id,
+                days_remaining=days_remaining,
+            )
+        texts = MIMEMultipart()
+        texts.attach(MIMEText(rich_text, "html"))
+        texts.attach(MIMEText(plain_text))
+        self.message.attach(texts)
