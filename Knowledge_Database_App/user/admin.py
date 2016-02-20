15ea0345b70ed33df4f7be1f090ec55d4dbb64ec
Lukas <Lukas>
2016-02-20 04:04:26 -0500

    Started Admin API

--- Knowledge_Database_App/user/admin.py
+++ Knowledge_Database_App/user/admin.py
@@ -2,16 +2,33 @@
 Admin API
 """
 
+from Knowledge_Database_App.storage import (orm_core as orm,
+                                            select_queries as select,
+                                            action_queries as action)
 from .user import RegisteredUser
 
 
 class Admin(RegisteredUser):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.user_type = "admin"
+        self.reports = None
+
+    @classmethod
+    def promote(cls, user_id):
+        try:
+            self.storage_handler.call(select.change_user_type, 
+                                      self.user_id, "admin")
+        except:
+            raise
 
     def demote(self):
-        pass
+        try:
+            self.storage_handler.call(select.change_user_type, 
+                                      self.user_id, "standard")
+        except:
+            raise
 
     @property
     def json_ready(self):
--- Knowledge_Database_App/user/report.py
+++ Knowledge_Database_App/user/report.py
@@ -6,6 +6,7 @@ from datetime import datetime, timedelta
 import dateutil.parser as dateparse
 
 from Knowledge_Database_App.content import redis
+from Knowledge_Database_App.content.edit import is_ip_address
 from Knowledge_Database_App.storage import (orm_core as orm,
                                             select_queries as select,
                                             action_queries as action)
@@ -13,15 +14,59 @@ from Knowledge_Database_App.storage import (orm_core as orm,
 
 class Report:
 
-    def __init__(self):
-        pass
+    storage_handler = orm.StorageHandler()
+
+    report_id = None        # Integer.
+    content_id = None       # Integer.
+    report_text = None      # String.
+    report_type = None      # String, accepts 'content' or 'authors'.
+    author_type = None      # String, accepts 'U' or IP address.
+    author_id = None        # Integer.
+    admin_report = None     # String.
+    admin_id = None         # Integer.
+    timestamp = None        # Datetime.
+    res_timestamp = None    # Datetime.
+
+    def __init__(self, report_id=None, content_id=None, report_text=None, 
+                 report_type=None, author_type=None, author_id=None):
+        if report_id is not None:
+            try:
+                report_object = self.storage_handler.call(
+                    select.get_user_reports, report_id=report_id)
+            except:
+                raise
+            else:
+                self._transfer(report_object)
+        else:
+            if not content_id or not report_text or not report_type or (
+                    author_type is not None and 
+                    not is_ip_address(author_type) and author_type != "U") or (
+                    author_type == "U" and author_id is None) or (
+                    report_type != "content" and report_type != "authors"):
+                raise select.InputError("Invalid arguments!")
+            else:
+                self.report_text = report_text.strip()
+                self.content_id = content_id
+                self.report_type = report_type
+                self.author_type = author_type
+                self.author_id = author_id
+                self.timestamp = datetime.utcnow()
 
     @staticmethod
     def _check_legal():
         pass
 
-    def _transfer(self):
-        pass
+    def _transfer(self, report_object):
+        self.report_id = report_object.report_id
+        self.report_text = report_object.report_text
+        self.report_type = report_object.report_type
+        self.author_type = report_object.author_type
+        self.admin_report = report_object.admin_report
+        self.timestamp = report_object.timestamp
+        self.res_timestamp = report_object.res_timestamp
+        self.author_id = report_object.author_id
+        self.admin_id = report_object.admin_id
+        self.content_id = report_object.content_id
 
     @classmethod
     def bulk_retrieve(cls):
@@ -33,7 +78,7 @@ class Report:
     def assign_admin(self):
         pass
 
-    def resolve(self):
+    def resolve(self, admin_report):
         pass
 
     def save(self):
