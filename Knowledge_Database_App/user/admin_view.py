da30fdc37c2a5f1465a5955f4b83a00f6cb45f61
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-02-23 22:49:44 -0500

    Completed view APIs

--- Knowledge_Database_App/user/admin_view.py
+++ Knowledge_Database_App/user/admin_view.py
@@ -2,10 +2,23 @@
 Admin View API
 """
 
-from . import admin
+from . import admin as admin_api
 
 
-class UserView:
+class AdminView:
+    """
+    Attributes:
+        admin: Dictionary representation of an Admin object.
+    """
 
-    def __init__(self):
-        pass
+    def __init__(self, user_id=None, email=None, password=None,
+                 user_name=None, remember_id=None, remember_token=None,
+                 remember_user=False):
+        try:
+            admin = admin_api.Admin(user_id=user_id, email=email,
+                password=password, user_name=user_name, remember_id=remember_id,
+                remember_token=remember_token, remember_user=remember_user)
+        except:
+            raise
+        else:
+            self.admin = admin.json_ready
--- Knowledge_Database_App/user/report_view.py
+++ Knowledge_Database_App/user/report_view.py
@@ -2,18 +2,48 @@
 Report View API
 """
 
-from . import report
+from . import report as report_api
 
 
 class ReportView:
+    """
+    Attributes:
+        report: Dictionary representation of a Report object.
+    Class Methods:
+        bulk_retrieve, resolve
+    """
 
-    def __init__(self):
-        pass
+    def __init__(self, report_id=None, report_status=None, content_id=None,
+                 report_text=None, report_type=None, author_type=None,
+                 author_id=None):
+        try:
+            report = report_api.Report(report_id=report_id,
+                report_status=report_status, content_id=content_id,
+                report_text=report_text, report_type=report_type,
+                author_type=author_type, author_id=author_id)
+        except:
+            raise
+        else:
+            report.submit()
+            self.report = report.json_ready
 
     @classmethod
-    def bulk_retrieve(cls):
-        pass
+    def bulk_retrieve(cls, user_id=None, admin_id=None, content_id=None,
+                      ip_address=None, report_status="open", page_num=1):
+        try:
+            reports = report_api.Report.bulk_retrieve(user_id=user_id,
+                admin_id=admin_id, content_id=content_id, ip_address=ip_address,
+                report_status=report_status, page_num=page_num)
+        except:
+            raise
+        else:
+            return reports
 
     @classmethod
-    def resolve(cls):
-        pass
+    def resolve(cls, report_id, report_status, admin_report):
+        try:
+            report = report_api.Report(report_id=report_id,
+                                       report_status=report_status)
+            report.resolve(admin_report)
+        except:
+            raise
