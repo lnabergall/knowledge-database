9ad35d9490358a7d9690a626b6e9887b6e48058d
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-02-26 00:10:32 -0500

    Started User API unit tests

---
+++ Knowledge_Database_App/tests/user/test_user.py
@@ -0,0 +1,24 @@
+"""
+User Unit Tests
+
+Currently primarily checks only core functionality of all
+functions, not exceptional cases.
+
+Note: Tests are numbered to force a desired execution order.
+"""
+
+from datetime import datetime
+from unittest import TestCase, skipIf
+
+from Knowledge_Database_App.user.user import RegisteredUser
+
+
+class UserTest(TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+
