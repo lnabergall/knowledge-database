52cb2eb5a9b981b7895ab41aad63c20c6e9c77d1
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-03-02 03:30:59 -0500

    Started User API unit tests

--- Knowledge_Database_App/tests/user/test_user.py
+++ Knowledge_Database_App/tests/user/test_user.py
@@ -16,9 +16,135 @@ from Knowledge_Database_App.user.user import RegisteredUser
 class UserTest(TestCase):
 
     def setUp(self):
-        pass
+        self.user_name = "Kylo Ren"
+        self.email = "kyloren@gmail.com"
+        self.password = "darthvader123"
+        self.failure = False
+        self.stored = False
 
     def tearDown(self):
-        pass
+        if not self.failure:
+            pass
 
+    @skipIf(self.failure, "Necessary previous test failed!")
+    def test_01_create(self):
+        try:
+            self.user = RegisteredUser(
+                email=self.email, password=self.password,
+                user_name=self.user_name)
+        except Exception as e:
+            self.failure = True
+            self.fail(str(e))
+        else:
+            try:
+                self.assertEqual(user.email, self.email)
+                self.assertEqual(user.user_name, self.user_name)
+                self.assertNotEqual(user.pass_hash, self.password)
+                self.assertIsInstance(user.pass_hash_type, str)
+                self.assertEqual(user.user_type, "standard")
+                self.assertIsNotNone(user.remember_id)
+                self.assertIsInstance(user.timestamp, datetime)
+            except AssertionError:
+                self.failure = True
+                raise
 
+    @skipIf(self.failure, "Necessary previous test failed!")
+    def test_02_register(self):
+        try:
+            self.user.register()
+        except Exception as e:
+            self.failure = True
+            self.fail(str(e))
+        else:
+            try:
+                self.assertIsInstance(self.user.user_id, int)
+            except AssertionError:
+                self.failure = True
+                raise
+
+    @skipIf(self.failure, "Necessary previous test failed!")
+    def test_03_login(self):
+        try:
+            user_from_id = RegisteredUser(user_id=self.user.user_id)
+            user_from_login = RegisteredUser(email=self.email,
+                                             password=self.password)
+        except Exception as e:
+            self.failure = True
+            self.fail(str(e))
+        else:
+            try:
+                self.assertEqual(self.user, user_from_id)
+                self.assertEqual(self.user, user_from_login)
+            except AssertionError:
+                self.failure = True
+                raise
+
+    @skipIf(self.failure, "Necessary previous test failed!")
+    def test_04_process_confirm(self):
+        try:
+            pass
+        except Exception as e:
+            self.failure = True
+            self.fail(str(e))
+        else:
+            try:
+                pass
+            except AssertionError:
+                self.failure = True
+                raise
+
+    @skipIf(self.failure, "Necessary previous test failed!")
+    def test_05_remember_user(self):
+        try:
+            pass
+        except Exception as e:
+            self.failure = True
+            self.fail(str(e))
+        else:
+            try:
+                pass
+            except AssertionError:
+                self.failure = True
+                raise
+
+    @skipIf(self.failure, "Necessary previous test failed!")
+    def test_06_update(self):
+        try:
+            pass
+        except Exception as e:
+            self.failure = True
+            self.fail(str(e))
+        else:
+            try:
+                pass
+            except AssertionError:
+                self.failure = True
+                raise
+
+    @skipIf(self.failure, "Necessary previous test failed!")
+    def test_07_json_ready(self):
+        try:
+            pass
+        except Exception as e:
+            self.failure = True
+            self.fail(str(e))
+        else:
+            try:
+                pass
+            except AssertionError:
+                self.failure = True
+                raise
+
+    @skipIf(self.failure, "Necessary previous test failed!")
+    def test_08_delete(self):
+        try:
+            pass
+        except Exception as e:
+            self.failure = True
+            self.fail(str(e))
+        else:
+            try:
+                pass
+            except AssertionError:
+                self.failure = True
+                raise
