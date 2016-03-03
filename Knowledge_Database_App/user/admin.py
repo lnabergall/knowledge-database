695d74908dc05da3a05fad39975470c5db7b6791
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-03-02 23:31:49 -0500

    Fixed init method

--- Knowledge_Database_App/user/admin.py
+++ Knowledge_Database_App/user/admin.py
@@ -6,9 +6,7 @@ Classes:
     Admin
 """
 
-from Knowledge_Database_App.storage import (orm_core as orm,
-                                            select_queries as select,
-                                            action_queries as action)
+from Knowledge_Database_App.storage import action_queries as action
 from .user import RegisteredUser
 from .report import Report
 
@@ -17,28 +15,32 @@ class Admin(RegisteredUser):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.user_type = "admin"
-        open_reports = Report.bulk_retrieve(admin_id=self.user_id,
-                                            report_status="open", page_num=0)
-        resolved_reports = Report.bulk_retrieve(admin_id=self.user_id,
-                                                report_status="resolved",
-                                                page_num=1)
-        self.reports = {
-            "open": open_reports,
-            "resolved": resolved_reports,
-        }
+        if kwargs["user_name"] is not None:
+            self.user_type = "admin"
+        else:
+            if self.user_type != "admin":
+                raise NotImplementedError
+            open_reports = Report.bulk_retrieve(admin_id=self.user_id,
+                                                report_status="open", page_num=0)
+            resolved_reports = Report.bulk_retrieve(admin_id=self.user_id,
+                                                    report_status="resolved",
+                                                    page_num=1)
+            self.reports = {
+                "open": open_reports,
+                "resolved": resolved_reports,
+            }
 
     @classmethod
     def promote(cls, user_id):
         try:
-            self.storage_handler.call(select.change_user_type, 
+            self.storage_handler.call(action.change_user_type,
                                       user_id, "admin")
         except:
             raise
 
     def demote(self):
         try:
-            self.storage_handler.call(select.change_user_type, 
+            self.storage_handler.call(action.change_user_type,
                                       self.user_id, "standard")
         except:
             raise
