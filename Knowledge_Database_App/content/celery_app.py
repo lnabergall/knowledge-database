6404966fdd607a7c62c99cccda166734ac8b19d2
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-03-24 00:04:38 -0400

    Add arg to Celery app

--- Knowledge_Database_App/content/celery_app.py
+++ Knowledge_Database_App/content/celery_app.py
@@ -5,5 +5,5 @@ Primary celery module. Holds the celery app instance.
 from celery import Celery
 
 
-celery_app = Celery()
+celery_app = Celery("celery_app")
 celery_app.config_from_object("Knowledge_Database_App.content.celery_config")
