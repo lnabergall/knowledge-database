f151c02d7beddb115463a101072ffdc7c9423608
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-03-26 01:47:02 -0400

    Fixed Celery configuration

--- Knowledge_Database_App/content/celery_config.py
+++ Knowledge_Database_App/content/celery_config.py
@@ -6,7 +6,7 @@ from kombu import Queue
 
 
 # Broker settings.
-BROKER_URL = "amqp://"
+BROKER_URL = "amqp://guest@localhost//"
 
 # Result task and backend (for storing errors) settings.
 CELERY_RESULT_BACKEND = "redis://"
@@ -18,8 +18,10 @@ CELERY_TRACK_STARTED = True
 CELERY_STORE_ERRORS_EVEN_IF_IGNORED = True
 
 # List of modules to import when celery starts.
-CELERY_IMPORTS = ["Knowledge_Database_App.user.user"]
+CELERY_IMPORTS = ["Knowledge_Database_App.user.user",
+                  "Knowledge_Database_App.content.edit"]
 
+# TODO: Fix routing setup
 # Queue settings.
 # CELERY_QUEUES = (
 #     Queue("default", routing_key="default"),
@@ -31,29 +33,36 @@ CELERY_IMPORTS = ["Knowledge_Database_App.user.user"]
 # CELERY_DEFAULT_QUEUE = "default"
 
 # Task routes.
-CELERY_ROUTES = {
-    "edit._notify": {
-        "queue": "email_tasks",
-        "routing_key": "email.edit._notify"
-    },
-    "edit.validate": {
-        "queue": "edit_tasks",
-        "routing_key": "edit.edit.validate"
-    },
-    "user.request_confirm": {
-        "queue": "email_tasks",
-        "routing_key": "email.user.request_confirm"
-    },
-    "user.expire_confirm": {
-        "queue": "user_tasks",
-        "routing_key": "user.user.expire_confirm"
-    },
-    "user.send_welcome": {
-        "queue": "email_tasks",
-        "routing_key": "email.user.send_welcome"
-    },
-
-}
+# CELERY_ROUTES = {
+#     "edit._notify": "email_tasks",
+#     "edit.validate": "edit_tasks",
+#     "user.request_confirm": "email_tasks",
+#     "user.expire_confirm": "user_tasks",
+#     "user.send_welcome": "email_tasks",
+# }
+# CELERY_ROUTES = {
+#     "edit._notify": {
+#         "queue": "email_tasks",
+#         "routing_key": "email.edit._notify"
+#     },
+#     "edit.validate": {
+#         "queue": "edit_tasks",
+#         "routing_key": "edit.edit.validate"
+#     },
+#     "user.request_confirm": {
+#         "queue": "email_tasks",
+#         "routing_key": "email.user.request_confirm"
+#     },
+#     "user.expire_confirm": {
+#         "queue": "user_tasks",
+#         "routing_key": "user.user.expire_confirm"
+#     },
+#     "user.send_welcome": {
+#         "queue": "email_tasks",
+#         "routing_key": "email.user.send_welcome"
+#     },
+#
+# }
 
 # File to store worker state (e.g. revoked tasks).
 CELERYD_STATE_DB = "celery_worker_state"
