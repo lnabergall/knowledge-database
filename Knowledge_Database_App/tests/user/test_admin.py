dcfa8d578afa8bbbd8028cc10e828ae444cad56e
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-06-09 01:54:35 -0400

    Fixed test setup and teardown and corresponding attributes

--- Knowledge_Database_App/tests/user/test_admin.py
+++ Knowledge_Database_App/tests/user/test_admin.py
@@ -8,7 +8,7 @@ Note: Tests are numbered to force a desired execution order.
 """
 
 from datetime import datetime
-from unittest import TestCase
+from unittest import TestCase, skipIf
 
 from Knowledge_Database_App.tests import skipIfTrue
 from Knowledge_Database_App.storage.action_queries import delete_user
@@ -17,110 +17,120 @@ from Knowledge_Database_App.user.admin import Admin
 
 
 class AdminTest(TestCase):
+    failure = False
+    stored = False
 
-    def setUp(self):
-        self.user_name = "Kylo Ren"
-        self.email = "kyloren121323@gmail.com"
-        self.password = "darthvader123"
-        self.failure = False
-        self.stored = False
+    @classmethod
+    def setUpClass(cls):
+        cls.user_name = "Kylo Ren"
+        cls.email = "kyloren121323@gmail.com"
+        cls.password = "darthvader123"
 
-    def tearDown(self):
-        if self.stored:
-            delete_user(self.admin.user_id, permanently=True)
+    @classmethod
+    def tearDownClass(cls):
+        if cls.stored:
+            Admin.storage_handler.call(
+                delete_user, cls.admin.user_id, permanently=True)
 
-    @skipIfTrue("failure")
+    @skipIf(failure, "Previous test failed!")
     def test_01_create(self):
         try:
-            self.admin = Admin(email=self.email, user_name=self.user_name,
-                               password=self.password)
+            self.__class__.admin = Admin(email=self.__class__.email,
+                                         user_name=self.__class__.user_name,
+                                         password=self.__class__.password)
             self.admin.register()
         except Exception as e:
-            self.failure = True
+            self.__class__.failure = True
             self.fail(str(e))
         else:
             try:
-                self.assertEqual(self.admin.email, self.email)
-                self.assertEqual(self.admin.user_name, self.user_name)
-                self.assertNotEqual(self.admin.pass_hash, self.password)
-                self.assertIsInstance(self.admin.pass_hash_type, str)
-                self.assertEqual(self.admin.user_type, "admin")
-                self.assertIsNotNone(self.admin.remember_id)
-                self.assertIsInstance(self.admin.timestamp, datetime)
-                self.assertIsInstance(self.admin.user_id, int)
+                self.assertEqual(self.__class__.admin.email,
+                                 self.__class__.email)
+                self.assertEqual(self.__class__.admin.user_name,
+                                 self.__class__.user_name)
+                self.assertNotEqual(self.__class__.admin.pass_hash,
+                                    self.__class__.password)
+                self.assertIsInstance(self.__class__.admin.pass_hash_type, str)
+                self.assertEqual(self.__class__.admin.user_type, "admin")
+                self.assertIsNotNone(self.__class__.admin.remember_id)
+                self.assertIsInstance(self.__class__.admin.timestamp, datetime)
+                self.assertIsInstance(self.__class__.admin.user_id, int)
             except AssertionError:
-                self.failure = True
+                self.__class__.failure = True
                 raise
             except Exception as e:
-                self.failure = True
+                self.__class__.failure = True
                 self.fail(str(e))
             else:
-                delete_user(self.admin.user_id, permanently=True)
+                delete_user(self.__class__.admin.user_id, permanently=True)
 
-    @skipIfTrue("failure")
+    @skipIf(failure, "Previous test failed!")
     def test_02_promote(self):
         try:
-            user = RegisteredUser(email=self.email, password=self.password,
-                                  user_name=self.user_name)
+            user = RegisteredUser(email=self.__class__.email,
+                                  password=self.__class__.password,
+                                  user_name=self.__class__.user_name)
             user.register()
             Admin.promote(user.user_id)
         except Exception as e:
-            self.failure = True
+            self.__class__.failure = True
             self.fail(str(e))
         else:
             try:
                 admin = Admin(user_id=user.user_id)
-                self.assertEqual(self.admin, admin)
+                self.assertEqual(self.__class__.admin, admin)
             except AssertionError:
-                self.failure = True
+                self.__class__.failure = True
                 raise
             except Exception as e:
-                self.failure = True
+                self.__class__.failure = True
                 self.fail(str(e))
             else:
-                self.admin = admin
-                self.stored = True
+                self.__class__.admin = admin
+                self.__class__.stored = True
 
-    @skipIfTrue("failure")
+    @skipIf(failure, "Previous test failed!")
     def test_03_demote(self):
         try:
-            self.admin.demote()
+            self.__class__.admin.demote()
         except Exception as e:
-            self.failure = True
+            self.__class__.failure = True
             self.fail(str(e))
         else:
             try:
-                user = RegisteredUser(user_id=self.admin.user_id)
+                user = RegisteredUser(user_id=self.__class__.admin.user_id)
                 self.assertEqual(user.user_type, "standard")
             except AssertionError:
-                self.failure = True
+                self.__class__.failure = True
                 raise
             except Exception as e:
-                self.failure = True
+                self.__class__.failure = True
                 self.fail(str(e))
 
-    @skipIfTrue("failure")
+    @skipIf(failure, "Previous test failed!")
     def test_04_json_ready(self):
         try:
-            json_ready_dict = self.admin.json_ready
+            json_ready_dict = self.__class__.admin.json_ready
         except Exception as e:
-            self.failure = True
+            self.__class__.failure = True
             self.fail(str(e))
         else:
             try:
-                self.assertEqual(json_ready_dict["user_id"], self.admin.user_id)
+                self.assertEqual(json_ready_dict["user_id"],
+                                 self.__class__.admin.user_id)
                 self.assertEqual(json_ready_dict["user_name"],
-                                 self.admin.user_name)
+                                 self.__class__.admin.user_name)
                 self.assertEqual(json_ready_dict["user_type"],
-                                 self.admin.user_type)
-                self.assertEqual(json_ready_dict["email"], self.admin.email)
+                                 self.__class__.admin.user_type)
+                self.assertEqual(json_ready_dict["email"],
+                                 self.__class__.admin.email)
                 self.assertEqual(json_ready_dict["timestamp"],
-                                 self.admin.timestamp)
+                                 self.__class__.admin.timestamp)
                 self.assertEqual(json_ready_dict["deleted_timestamp"],
-                                 self.admin.deleted_timestamp)
+                                 self.__class__.admin.deleted_timestamp)
             except AssertionError:
-                self.failure = True
+                self.__class__.failure = True
                 raise
             except Exception as e:
-                self.failure = True
+                self.__class__.failure = True
                 self.fail(str(e))
--- Knowledge_Database_App/tests/user/test_report.py
+++ Knowledge_Database_App/tests/user/test_report.py
@@ -17,55 +17,58 @@ from Knowledge_Database_App.user.report import Report
 
 
 class ReportTest(TestCase):
+    failure = False
+    stored = False
 
-    def setUp(self):
-        self.content_id = 1
-        self.report_text = "There is a problem."
-        self.report_type = "content"
-        self.author_type = "104.156.240.197"
-        self.admin = Admin(email="kyloren121323@gmail.com",
+    @classmethod
+    def setUpClass(cls):
+        cls.content_id = 1
+        cls.report_text = "There is a problem."
+        cls.report_type = "content"
+        cls.author_type = "104.156.240.197"
+        cls.admin = Admin(email="kyloren121323@gmail.com",
                            password="darthvader123",
                            user_name="Kylo Ren")
-        self.admin.register()
-        self.admin_report = "The problem was that, I fixed it by doing this."
-        self.failure = False
-        self.stored = False
+        cls.admin.register()
+        cls.admin_report = "The problem was that, I fixed it by doing this."
 
-    def tearDown(self):
-        if self.stored:
-            delete_user(self.admin.user_id, permanently=True)
+    @classmethod
+    def tearDownClass(cls):
+        if cls.stored:
+            Admin.storage_handler.call(
+                delete_user, cls.admin.user_id, permanently=True)
 
-    @skipIfTrue("failure")
+    @skipIf(failure, "Previous test failed!")
     def test_01_create(self):
         try:
-            self.report = Report(content_id=self.content_id,
-                                 report_text=self.report_text,
-                                 report_type=self.report_type,
-                                 author_type=self.author_type)
+            self.report = Report(content_id=self.__class__.content_id,
+                                 report_text=self.__class__.report_text,
+                                 report_type=self.__class__.report_type,
+                                 author_type=self.__class__.author_type)
         except Exception as e:
-            self.failure = True
+            self.__class__.failure = True
             self.fail(str(e))
         else:
             try:
-                self.assertEqual(self.report.report_text, self.report_text)
-                self.assertEqual(self.report.content_id, self.content_id)
-                self.assertEqual(self.report.report_type, self.report_type)
+                self.assertEqual(self.report.report_text, self.__class__.report_text)
+                self.assertEqual(self.report.content_id, self.__class__.content_id)
+                self.assertEqual(self.report.report_type, self.__class__.report_type)
                 self.assertEqual(self.report.report_status, "pending")
-                self.assertEqual(self.report.author_type, self.author_type)
+                self.assertEqual(self.report.author_type, self.__class__.author_type)
                 self.assertIsInstance(self.report.timestamp, datetime)
             except AssertionError:
-                self.failure = True
+                self.__class__.failure = True
                 raise
             except Exception as e:
-                self.failure = True
+                self.__class__.failure = True
                 self.fail(str(e))
 
-    @skipIfTrue("failure")
+    @skipIf(failure, "Previous test failed!")
     def test_02_submit(self):
         try:
             self.report.submit()
         except Exception as e:
-            self.failure = True
+            self.__class__.failure = True
             self.fail(str(e))
         else:
             try:
@@ -73,34 +76,34 @@ class ReportTest(TestCase):
                 self.assertEqual(self.report.report_status, "open")
                 self.assertIsInstance(self.report.report_id, int)
             except AssertionError:
-                self.failure = True
+                self.__class__.failure = True
                 raise
             except Exception as e:
-                self.failure = True
+                self.__class__.failure = True
                 self.fail(str(e))
             else:
-                self.stored = True
+                self.__class__.stored = True
 
-    @skipIfTrue("failure")
+    @skipIf(failure, "Previous test failed!")
     def test_03_bulk_retrieve(self):
         try:
             admin_reports = Report.bulk_retrieve(
-                admin_id=self.admin.user_id, page_num=0)
+                admin_id=self.__class__.admin.user_id, page_num=0)
             content_reports = Report.bulk_retrieve(
-                content_id=self.content_id, page_num=0)
+                content_id=self.__class__.content_id, page_num=0)
             author_reports = Report.bulk_retrieve(
-                ip_address=self.author_type, page_num=0)
+                ip_address=self.__class__.author_type, page_num=0)
             admin_reports_resolved = Report.bulk_retrieve(
-                admin_id=self.admin.user_id,
+                admin_id=self.__class__.admin.user_id,
                 report_status="resolved", page_num=0)
             content_reports_resolved = Report.bulk_retrieve(
-                content_id=self.content_id,
+                content_id=self.__class__.content_id,
                 report_status="resolved", page_num=0)
             author_reports_resolved = Report.bulk_retrieve(
-                ip_address=self.author_type,
+                ip_address=self.__class__.author_type,
                 report_status="resolved", page_num=0)
         except Exception as e:
-            self.failure = True
+            self.__class__.failure = True
             self.fail(str(e))
         else:
             try:
@@ -120,49 +123,49 @@ class ReportTest(TestCase):
                 for report in author_reports_resolved:
                     self.assertIsInstance(report, Report)
             except AssertionError:
-                self.failure = True
+                self.__class__.failure = True
                 raise
             except Exception as e:
-                self.failure = True
+                self.__class__.failure = True
                 self.fail(str(e))
 
-    @skipIfTrue("failure")
+    @skipIf(failure, "Previous test failed!")
     def test_04_resolve(self):
         try:
-            self.report.resolve(self.admin_report)
+            self.report.resolve(self.__class__.admin_report)
         except Exception as e:
-            self.failure = True
+            self.__class__.failure = True
             self.fail(str(e))
         else:
             try:
-                self.assertEqual(self.report.admin_report, self.admin_report)
+                self.assertEqual(self.report.admin_report, self.__class__.admin_report)
                 self.assertIsInstance(self.report.res_timestamp, datetime)
                 self.assertEqual(self.report.report_status, "resolved")
                 admin_reports_resolved = Report.bulk_retrieve(
-                    admin_id=self.admin.user_id,
+                    admin_id=self.__class__.admin.user_id,
                     report_status="resolved", page_num=0)
                 content_reports_resolved = Report.bulk_retrieve(
-                    content_id=self.content_id,
+                    content_id=self.__class__.content_id,
                     report_status="resolved", page_num=0)
                 author_reports_resolved = Report.bulk_retrieve(
-                    ip_address=self.author_type,
+                    ip_address=self.__class__.author_type,
                     report_status="resolved", page_num=0)
                 self.assertIn(self.report, admin_reports_resolved)
                 self.assertIn(self.report, content_reports_resolved)
                 self.assertIn(self.report, author_reports_resolved)
             except AssertionError:
-                self.failure = True
+                self.__class__.failure = True
                 raise
             except Exception as e:
-                self.failure = True
+                self.__class__.failure = True
                 self.fail(str(e))
 
-    @skipIfTrue("failure")
+    @skipIf(failure, "Previous test failed!")
     def test_05_json_ready(self):
         try:
             json_ready_dict = self.report.json_ready
         except Exception as e:
-            self.failure = True
+            self.__class__.failure = True
             self.fail(str(e))
         else:
             try:
@@ -189,8 +192,8 @@ class ReportTest(TestCase):
                 self.assertEqual(json_ready_dict["res_timestamp"],
                                  self.report.res_timestamp)
             except AssertionError:
-                self.failure = True
+                self.__class__.failure = True
                 raise
             except Exception as e:
-                self.failure = True
+                self.__class__.failure = True
                 self.fail(str(e))
