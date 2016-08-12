c60105976ff63902ba453106f08e22973343a1ed
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-08-12 03:21:51 -0400

    Added get_vote_results class method that returns a high-level summary of the vote

--- Knowledge_Database_App/content/vote_view.py
+++ Knowledge_Database_App/content/vote_view.py
@@ -23,24 +23,51 @@ class DuplicateVoteError(redis_api.DuplicateVoteError):
 
 class VoteView:
 
-    def __init__(self, vote_status, edit_id, vote, voter_id,
-                 timestamp=None, close_timestamp=None):
+    def __init__(self, vote_status=None, edit_id=None, vote=None,
+                 voter_id=None, timestamp=None, close_timestamp=None):
         """
         Args:
             vote_status: String, accepts 'in-progress' or 'ended'.
-            edit_id: Integer.
-            vote: String, expects 'Y' or 'N'.
-            voter_id: Integer.
+                Defaults to None.
+            edit_id: Integer. Defaults to None.
+            vote: String, expects 'Y' or 'N'. Defaults to None.
+            voter_id: Integer. Defaults to None.
             timestamp: Datetime. Defaults to None.
             close_timestamp: Datetime. Defaults to None.
         """
-        try:
-            vote = AuthorVote(vote_status, edit_id, vote, voter_id,
-                              timestamp, close_timestamp)
-            self.error_response = vote.save()
-        except redis_api.DuplicateVoteError as e:
-            raise DuplicateVoteError(str(e))
-        except:
-            raise
-        else:
-            self.vote = vote.json_ready
+        if vote:
+            try:
+                vote = AuthorVote(vote_status, edit_id, vote, voter_id,
+                                  timestamp, close_timestamp)
+                self.error_response = vote.save()
+            except redis_api.DuplicateVoteError as e:
+                raise DuplicateVoteError(str(e))
+            except:
+                raise
+            else:
+                self.vote = vote.json_ready
+
+    @classmethod
+    def get_vote_results(cls, vote_status, edit_id, validation_status=None):
+        """
+        Args:
+            vote_status: String, expects 'in-progress' or 'ended'.
+            edit_id: Integer.
+            validation_status: String, expects 'validating', 'accepted',
+                or 'rejected'. Defaults to None.
+        Returns:
+            Dictionary of the form
+            {'Y': int, 'N': int}
+        """
+        votes = AuthorVote.bulk_retrieve(vote_status, edit_id=edit_id,
+                                         validation_status=validation_status)
+        for_count = 0
+        against_count = 0
+        for author_vote in votes:
+            if author_vote.vote == "Y":
+                for_count += 1
+            else:
+                against_count += 1
+
+        return {"Y": for_count, "N": against_count}
+
