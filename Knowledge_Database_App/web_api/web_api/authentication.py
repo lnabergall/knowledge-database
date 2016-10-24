af2d98828adfbcc115173f06c5c116e39a6befad
Lukas Nabergall <lnabergall@mail.usf.edu>
2016-10-24 03:55:31 -0400

    Added functions for remembering authentication credentials

--- Knowledge_Database_App/web_api/web_api/authentication.py
+++ Knowledge_Database_App/web_api/web_api/authentication.py
@@ -3,6 +3,7 @@ Authentication objects and settings.
 """
 
 from pyramid.authentication import SessionAuthenticationPolicy
+from pyramid.security import _get_authentication_policy
 
 from Knowledge_Database_App.content.edit import is_ip_address
 
@@ -14,4 +15,21 @@ class CustomSessionAuthPolicy(SessionAuthenticationPolicy):
         self.authenticated_userid_key = self.prefix + "authenticated_userid"
 
     def authenticated_userid(self, request):
-        return int(request.session.get(self.authenticated_userid_key))
+        stored_id = request.session.get(self.authenticated_userid_key)
+        try:
+            return int(stored_id)
+        except TypeError:
+            return stored_id
+
+    def remember_authenticated(self, request, userid, **kw):
+        self.remember(request, userid, **kw)
+        request.session[self.authenticated_userid_key] = userid
+        return []
+
+
+def remember_authenticated(request, userid, **kw):
+        policy = _get_authentication_policy(request)
+        if policy is None:
+            return []
+        else:
+            return policy.remember_authenticated(request, userid, **kw)
